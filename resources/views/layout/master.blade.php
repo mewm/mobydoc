169eb8e0ff48cc125758d96d0db5ab2296015139
Dennis Micky Jensen <root@mewm.org>
2014-10-04 18:46:18 +0200

    Adding foundation

---
+++ resources/views/layout/master.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+	<link href='http://fonts.googleapis.com/css?family=Roboto:400,400italic,300italic,300' rel='stylesheet' type='text/css'>
+	<meta charset="UTF-8">
+	<title></title>
+</head>
+<body>
+
+</body>
+</html>
